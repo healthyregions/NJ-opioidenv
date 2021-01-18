6d74cea004ed1b2fe9f8063d1dabed4d33501485
Deckart2 <gdm100@me.com>
2021-01-18 11:35:05 -0600

    struggling to make business_types code work, will continue in a bit

--- code/business_types.R
+++ code/business_types.R
@@ -22,87 +22,50 @@ library(vegan)
 #Reads text data and only selects potentially relevant columns 
 #Also filters only for NJ and adds a geometry column
 read_data <- function(location_of_file, data_year = "2014") {
-  if (data_year == "2014"){
-    print("2014 route")
   df <- fread(location_of_file, 
               colClasses=c(
-                "logical", "character",
-                "character", "character",
-                "logical", "logical",
-                "logical", "integer",
-                "integer", "character",
-                "character", "integer", 
-                "character", "integer",
                 "character", "NULL",
+                "NULL", "character",
+                "NULL", "NULL",
+                "NULL", "NULL",
+                "NULL", "NULL",
+                "NULL", "NULL", 
+                "NULL", "integer",
+                "NULL", "NULL",
                 "NULL", "NULL" ,
                 "NULL", "NULL" ,
                 "NULL", "NULL" ,
                 "NULL", "NULL" ,
                 "NULL", "integer" ,
-                "NULL", "integer", 
-                "integer", "integer", 
-                "character", "NULL", 
-                "boolean", "NULL",
-                "integer", "NULL",
+                "NULL", "integer", #28
+                "integer", "NULL", 
+                "NULL", "NULL", 
                 "NULL", "NULL",
                 "NULL", "NULL",
                 "NULL", "NULL",
                 "NULL", "NULL",
                 "NULL", "NULL",
-                "logical", "logical", 
-                "logical", "logical",
-                "logical", "logical",
-                "logical"), 
+                "NULL", "NULL",
+                "NULL", "NULL",
+                "float64", "float64", 
+                "NULL", "NULL",
+                "NULL", "NULL",
+                "NULL"), 
               showProgress = TRUE)
-  df <- df[State == "NJ", ]
+  df <- df[df$State == "NJ", ]
   df <- df %>%
     as.data.frame() %>%
     st_as_sf(coords = c("Longitude", "Latitude"))
-  }
-  else {
-    df <- fread(location_of_file, 
-                colClasses=c("character", "NULL", 
-                             "NULL", "character", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "integer", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "integer", 
-                             "integer", "NULL", 
-                             "NULL", "NULL", 
-                             "logical", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL", "NULL", 
-                             "NULL"), 
-              showProgress = TRUE)
-  }
-  df <- df[State == "NJ", ]
-  df <- df %>%
-    as.data.frame()
 }
 
 #Call Function:
 njbd2014 <- read_data("data_raw/2014_Business_Academic_QCQ.txt")
-njbd2018 <- read_data("data_raw/2018_Business_Academic_QCQ.txt", data_year = "2018")
+njbd2018 <- read_data("data_raw/2018_Business_Academic_QCQ.txt")
 
 
 #Write 
-st_write(njbd2014, "data_in_progress/njbd2014.geojson")
-st_write(njbd2018, "data_in_progress/njbd2018.geojson")
+#st_write(njbd2014, "data_in_progress/njbd2014.geojson")
+#st_write(njbd2018, "data_in_progress/njbd2018.geojson")
 
 
 ## PART 2: SIMPLIFY DF AND CREATE COUNT FOR 2 DIGIT NAICS BY MUNICIPALITY ======
@@ -113,24 +76,17 @@ mun <- st_read("data_in_progress/mun_boundaries.geojson") %>%
   st_transform(3424)
 
 #Function that cleans data by selecting relevant columns (naics code, sales volume, employee size)
-clean_add_mun <- function(df, data_year = "2014") {
-  if (data_year=="2014") {
-    print("2014 branch")
+clean_add_mun <- function(df) {
+  st_crs(df) <- 3424
+  
     df <- df %>%
-    select(Company, naics9 = Primary.NAICS.Code, sales_volume = Sales.Volume..9....Location, 
-           emp_size = Employee.Size..5....Location, office_size = Office.Size.Code)
-  } else {
-    df <- df %>%
-      select(Company, naics9 = "Primary NAICS Code", sales_volume = "Sales Volume (9) - Location", 
-             emp_size = "Employee Size (5) - Location")
-  }
-  df %>%
-    mutate(naics_2 = str_sub(naics9, start = 1L, end = 2L)) %>% 
-    st_as_sf()%>%
-    st_transform(crs = st_crs(mun)) %>% #sets crs
-    st_join(mun) %>%#Adds municipality via spatial join
-    as.data.frame() %>% #Converts to dataframe
-    select(-geometry)
+    select(Company, naics9 = "Primary NAICS Code", sales_volume = "Sales Volume (9) - Location", 
+           emp_size = "Employee Size (5) - Location") %>%
+    mutate(naics_2 = str_sub(naics9, start = 1L, end = 2L)) %>%
+    st_transform(3424) %>%
+    st_join(mun) #%>%#Adds municipality via spatial join
+    #as.data.frame() %>% #Converts to dataframe
+    #select(-geometry)
 }
 
 #Function to count the number of businesses in each municipality and to find what percent each
@@ -143,8 +99,9 @@ bus_naics_count <- function(df) {
     mutate(naics_2 = paste0("pct_naics_2_", naics_2)) %>% #Renames naics2 to pct_naics_2_{naicscode} 
     pivot_wider(names_from = naics_2, values_from = n, values_fill = 0) %>% #Reshapes to organize by mun
     mutate(all_bus = rowSums(across(pct_naics_2_21:pct_naics_2_NA))) %>% #Gets new column with sum of all businesses in the mun
-    mutate(across(starts_with("pct_naics"), {~.x / all_bus})) #Converts rows from counts to percent 
-  
+    mutate(across(starts_with("pct_naics"), {~.x / all_bus})) #%>% #Converts rows from counts to percent 
+    #mutate(across(starts_with("pct_naics"), ~replace(., .==NaN, 0))) %>%
+    #mutate(across(starts_with("pct_naics"), ~replace(., .==Inf, 0))) 
 }
 
 #Function that computes each municipality's mean and median employee size and sales volume 
@@ -159,18 +116,18 @@ bus_size_emp <- function(df) {
 }
 
 #Read Data Previously Written:
-njbd2014 <- st_read("data_in_progress/njbd2014.geojson")
-njbd2018 <- st_read("data_in_progress/njbd2018.geojson")
+#njbd2014 <- st_read("data_in_progress/njbd2014.geojson")
+#njbd2018 <- st_read("data_in_progress/njbd2018.geojson")
 
 
 #Call functions:
-njbd2014 <- clean_add_mun(njbd2014)
-nj_naics2014 <- bus_naics_count(njbd2014)
-nj_buscount2014 <-bus_size_emp(njbd2014)
+njbd2014_clean_mun <- clean_add_mun(njbd2014)
+nj_naics2014 <- bus_naics_count(njbd2014_clean_mun)
+nj_buscount2014 <-bus_size_emp(njbd2014_clean_mun)
 
-njbd2018 <- clean_add_mun(njbd2018, data_year = "2018")
-nj_naics2018 <- bus_naics_count(njbd2018)
-nj_buscount2018 <-bus_size_emp(njbd2018)
+njbd2018_clean_mun <- clean_add_mun(njbd2018, data_year = "2018")
+nj_naics2018 <- bus_naics_count(njbd2018_clean_mun)
+nj_buscount2018 <-bus_size_emp(njbd2018_clean_mun)
 
 
 
