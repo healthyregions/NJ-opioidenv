e0e115622ba83a900787225a2927a9edd4566cd9
cavillanueva1 <85634871+cavillanueva1@users.noreply.github.com>
2021-08-09 21:39:47 -0700

    Revert "various code + master merges"
    This reverts commit bc46b246114fe77b170a0b3cbb07620bf6fc79c3.

--- code/MRI.merge.R
+++ code/MRI.merge.R
@@ -6,12 +6,7 @@ MRI <- read.csv("MRI.csv")
 
 MRI <- rename(MRI, SSN = X)
 MRI <- rename(MRI, percent_SNAP = per_snap)
-MRI <- rename(MRI, avg_property_tax = avg_prop_tax)
-
-
-
-MRI$percent_SNAP <- as.numeric(substr(MRI$percent_SNAP, 1, 4))
-
+MRI <- rename(MRi, avg_property_tax = avg_per_snap)
 
 MRI$Municipality = NULL
 
@@ -20,9 +15,6 @@ MRI$Municipality = NULL
 setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
 master <- read.csv("master_clean.csv")
 
-master$avg_prop_tax = NULL
-master$percent_SNAP = NULL
-master$MRI.Score = NULL
 
 master_updated <- merge(master, MRI, by = "SSN")
 
