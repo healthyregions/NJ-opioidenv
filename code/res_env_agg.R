1d245502f29b0ece5fc0ab8676357ed224b08511
cavillanueva1 <85634871+cavillanueva1@users.noreply.github.com>
2021-08-09 21:47:05 -0700

    res env and nrtc agg

---
+++ code/res_env_agg.R
@@ -0,0 +1,89 @@
+
+### Residential environment
+
+setwd("~/Documents/GitHub/NJ-opioidenv/data_in_progress")
+residential_environment <- read.csv("residential_environment.csv")
+
+### Read in crosswalk
+setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
+
+cw <- read.csv("cw_areal_interpolation.csv")
+cw <- rename(cw, GEOID = TRACTID)
+
+###
+
+### merge crosswalk and distance matrix
+cw_merge <- merge(residential_environment, cw, by = "GEOID")
+
+### new
+
+sub <- c("SSN", "pop_own_20yrs_plus", "pop_ren_20yrs_plus", "prop_of_ct")
+
+
+new <- cw_merge[sub]
+
+new$total <- new$pop_own_20yrs_plus + new$pop_ren_20yrs_plus
+
+new$weighted <- new$total * new$prop_of_ct
+
+
+final <- aggregate(new$weighted, by=list(SSN=new$SSN), FUN=sum)
+final <- rename(final, num_households_20yrs = x)
+
+
+### Percent renter
+
+
+# merge crosswalk and distance matrix
+cw_merge2 <- merge(residential_environment, cw, by = "GEOID")
+
+
+#subset
+
+sub2 <- c("SSN", "occupied_renter", "occupied_units", "prop_of_ct")
+
+rent <- cw_merge2[sub2]
+
+
+rent$percent_renter <- rent$occupied_renter / rent$occupied_units
+
+final2 <- aggregate(rent$percent_renter, by=list(SSN=new$SSN), FUN=sum)
+final2 <- rename(final2, percent_renter = x)
+
+
+
+
+### merge with 20 year occupancy
+
+final <- merge(final, final2, by = "SSN")
+
+final$occupied_renter = NULL
+
+final$occupied_units= NULL
+
+final$prop_of_ct = NULL
+
+
+### Save
+
+setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
+
+write.csv(final, "res_env.csv", row.names = FALSE) 
+
+
+#__________________
+
+
+### Merge to master
+
+setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
+re <- read.csv("res_env.csv")
+master <- read.csv("master_clean.csv")
+
+master_updated <- merge(master, re, by = "SSN")
+
+##save
+setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
+
+
+write.csv(master_updated, "master_clean.csv", row.names = FALSE) 
