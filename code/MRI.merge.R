d060e4db1124ad820c8e3027d9494bba830adff7
cavillanueva1 <85634871+cavillanueva1@users.noreply.github.com>
2021-08-09 21:38:13 -0700

    Revert "revert to former master"
    This reverts commit d749dc4bcb6d11574135d9ce81c39f73a1303e05.

--- code/MRI.merge.R
+++ code/MRI.merge.R
@@ -6,7 +6,12 @@ MRI <- read.csv("MRI.csv")
 
 MRI <- rename(MRI, SSN = X)
 MRI <- rename(MRI, percent_SNAP = per_snap)
-MRI <- rename(MRi, avg_property_tax = avg_per_snap)
+MRI <- rename(MRI, avg_property_tax = avg_prop_tax)
+
+
+
+MRI$percent_SNAP <- as.numeric(substr(MRI$percent_SNAP, 1, 4))
+
 
 MRI$Municipality = NULL
 
@@ -15,6 +20,9 @@ MRI$Municipality = NULL
 setwd("~/Documents/GitHub/NJ-opioidenv/data_final")
 master <- read.csv("master_clean.csv")
 
+master$avg_prop_tax = NULL
+master$percent_SNAP = NULL
+master$MRI.Score = NULL
 
 master_updated <- merge(master, MRI, by = "SSN")
 
